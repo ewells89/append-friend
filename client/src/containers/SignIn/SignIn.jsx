025286bd21973777588518ae2fa0e39ba4bf5e5e
Garrett Lee <rgl10d@gmail.com>
2021-02-03 21:37:39 -0500

    created and styled sign in page

---
+++ client/src/containers/SignIn/SignIn.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./SignIn.css";
+
+const SignIn = () => {
+  return (
+    <>
+    <h4 style={{fontFamily: "monospace", textAlign: "center"}}>.append(friend)</h4>
+      <div className="row input-fields">
+        <p id="please-sign-in">Please sign in below.</p>
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="username"
+                className="validate"
+                placeholder="Username"
+              />
+            </div>
+            <div className="col s12">
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    id="password"
+                    className="validate"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <button id="sign-in-button" className="waves-effect waves-dark btn">Sign in</button>
+          <button id="sign-up-button" className="waves-effect waves-dark btn">Sign up</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
