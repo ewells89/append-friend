b5b1f5608e68126314faf5ed68fb7544946f5b57
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-02-03 21:14:58 -0500

    starter code done

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(express.static("client/build"));
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/append-friend",
@@ -34,6 +37,10 @@ app.get("/api/config", (req,res) => {
         success: true,
     });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
