7ae30cf40f40b0d98f2f1bf43cfdd0cad9c4087b
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-02-06 12:17:05 -0500

    Added Schema

---
+++ models/developers.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+// const languageSchema = new Schema({name:String});
+
+const developersSchema = new Schema ({
+    username: {
+        type: String,
+        required: "Enter a Username",
+        unique: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is Required",
+        validate: [({ length }) => length >= 6, "Password should be longer."],
+    },
+    name: {
+        type: String,
+        required: "Enter your Name",
+        trim: true,
+    },
+    gitHub: {
+        type: String,
+        trim: true,
+        
+    },
+    languages: 
+        // array
+        [{type:String}],
+    email:{
+        type:String,
+        unique: true,
+        match:[/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    phone:{
+        type: Number,
+        validate: [({ length }) => length >= 10, "Phone number can't exceed 10 numbers."],
+    },
+    experience:{
+        type: Number,
+    },
+    location:{
+        type: String,
+        trim: true,
+    },
+    occupation:{
+        type: String,
+        trim: true,
+    },
+    appRole: {
+        type: String,
+        trim: true,
+    },
+    followedUsers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "developersSchema",
+    }],
+    usersFollowing:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "developersSchema",
+    }],
+});
+
+const Developers = mongoose.model("Developers", developersSchema);
+
+module.exports = Developers
