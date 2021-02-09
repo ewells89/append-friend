8086cf6081a210e590005ce54bc684e55fdd7ba2
Garrett Lee <rgl10d@gmail.com>
2021-02-08 20:10:55 -0500

    dev cards now generated with database data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./containers/SignIn/SignIn";
 import SignUp from "./containers/SignUp/SignUp";
 import Home from "./containers/Home/Home";
@@ -9,13 +9,13 @@ import Developers from "./containers/Developers/Developers";
 function App() {
   return (
     <Router>
-      <div>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/developers" component={Developers} />
         <Footer/>
-      </div>
+      </Switch>
     </Router>
   );
 }
--- client/src/containers/Developers/Developers.jsx
+++ client/src/containers/Developers/Developers.jsx
@@ -1,61 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import DevCards from "../../components/DevCards/DevCards";
+import axios from "axios";
 
 const Developers = () => {
-    const [developers, setDevelopers] = useState([
-        {
-            username: "jdeer74",
-            name: "John Deer",
-            gitHub: "@jdeer74",
-            languages: ["JavaScript","HTML5","CSS"],
-            phone: 5555555555,
-            experience: 15,
-            location: "Atlanta, GA",
-            occupation:"Web Developer",
-            appRole: "Mentor"
-        },
-        {
-            username: "afox87",
-            name: "Alicia Fox",
-            gitHub: "@afox87",
-            languages: ["JavaScript","HTML5","CSS"],
-            phone: 5555555555,
-            experience: 15,
-            location: "Atlanta, GA",
-            occupation:"Web Developer",
-            appRole: "Mentee"
-        },
-        {
-            username: "jdeer73",
-            name: "John Deer",
-            gitHub: "@jdeer73",
-            languages: ["JavaScript","HTML5","CSS"],
-            phone: 5555555555,
-            experience: 15,
-            location: "Atlanta, GA",
-            occupation:"Web Developer",
-            appRole: "Mentor"
-        },
-    ])
+  const [developers, setDevelopers] = useState([]);
 
-    // useEffect(() => {
-    //     },
-    //     []
-    // )
+  useEffect(() => {
+    getDevs()
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div className="row">
-                    {developers.map((developer) => {
-                        return <DevCards {...developer} key={developer.username}/>
-                    })}
-                </div>
-            </div>
-        </>
-    );
+  const getDevs = () => {
+    axios
+    .get("/api/developer")
+    .then((response) => {
+      console.log(response.data);
+      setDevelopers(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          {developers.map((developer) => {
+            return <DevCards {...developer} key={developer.username} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Developers;
