5041f5eb67d6b8c188926394d22990383c16f901
Garrett Lee <rgl10d@gmail.com>
2021-02-10 21:39:02 -0500

    created new tabs for navtabs

--- client/src/components/NavTabs/NavTabs.jsx
+++ client/src/components/NavTabs/NavTabs.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./NavTabs.css";
 
 const NavTabs = () => {
   return (
-    <ul className="tabs z-depth-1">
-      <li className="tab">
-        <a href="/developers">Developers</a>
-      </li>
-      <li className="tab disabled">
-        <a href="/">Projects</a>
-      </li>
-    </ul>
+    <div class="col s12">
+      <ul class="tabs tabs-icon">
+        <li class="tab col s3"><a href="/developers"><i class="material-icons">people</i>Developers</a></li>
+        {/* <li class="tab col s3"><a href="#test2"><i class="material-icons">computer</i>Projects</a></li> */}
+        <li class="tab col s3"><a href="/profile"><i class="material-icons">account_box</i>Profile</a></li>
+        <li class="tab col s3 right"><a href="#test4"><i class="material-icons">close</i>Sign Out</a></li>
+      </ul>
+    </div>
   );
 };
 
