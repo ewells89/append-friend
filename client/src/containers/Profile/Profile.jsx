2dcd8dfbafd5cc6c6396a706e3893c4b823f0cc8
Elyse Wells <cewells89@gmail.com>
2021-02-10 21:06:36 -0500

    finalized form fields on profile page.

--- client/src/containers/Profile/Profile.jsx
+++ client/src/containers/Profile/Profile.jsx
@@ -22,13 +22,8 @@ const Profile = () => {
                         <div className="col s6" id="profilePageColumn">
                             
                             <div className="input-field">
-                            <input id="first_name" type="text" className="validate"/>
-                            <label for="first_name">First Name</label>
-                            </div>
-
-                            <div className="input-field">
-                            <input id="last_name" type="text" className="validate"/>
-                            <label for="last_name">Last Name</label>
+                            <input id="name" type="text" className="validate"/>
+                            <label for="name">Name</label>
                             </div>
 
                             <div className="input-field">
@@ -42,13 +37,13 @@ const Profile = () => {
                             </div>
 
                             <div className="input-field">
-                            <input id="title" type="text" className="validate"/>
-                            <label for="title">Job Title</label>
+                            <input id="experience" type="text" className="validate"/>
+                            <label for="experience">Years of Experience</label>
                             </div>
                             
                             <div className="input-field">
-                            <input id="experience" type="text" className="validate"/>
-                            <label for="experience">Years of Experience</label>
+                            <input id="occupation" type="text" className="validate"/>
+                            <label for="occupation">Occupation/Job Title</label>
                             </div>
 
                             <div className="input-field">
@@ -59,26 +54,24 @@ const Profile = () => {
                         </div>
 
                         <div className="col s6" id="profilePageColumn">
-                            <label>Role</label>
-                                <select className="browser-default">
-                                    <option value="" disabled selected>Choose your role</option>
-                                    <option value="1">Mentor</option>
-                                    <option value="2">Mentee</option>
-                                    <option value="3">Collaborator</option>
-                                </select>
-
-                            <div class="input-field">
-                            <label>Languages</label>
-                                <select multiple>
-                                <option value="" disabled selected>Select languages</option>
-                                <option value="1">JavaScript</option>
-                                <option value="2">Angular</option>
-                                <option value="3">React</option>
-                                <option value="4">Vue</option>
-                                
-                                </select>
-                                
+                            <div class="row">
+                                <label>Role</label>
+                                    <select className="browser-default" id="appRole">
+                                        <option value="" disabled selected>Choose your role</option>
+                                        <option value="1">Mentor</option>
+                                        <option value="2">Mentee</option>
+                                        <option value="3">Collaborator</option>
+                                    </select>
                             </div>
+                            
+                            <div class="row">
+                                <p>Languages</p>
+                                    <div class="input-field">
+                                    <textarea id="languages" class="materialize-textarea"></textarea>
+                                    <label for="languages">Enter your regularly used languages here.</label>
+                                    </div>
+                            </div>
+
                         </div>
 
                     </form>
