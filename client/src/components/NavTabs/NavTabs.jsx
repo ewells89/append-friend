c3f6b6f88c45b8d660930223e96bc051a9300076
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-02-11 20:12:36 -0500

    Changed Class to className in navtabs

--- client/src/components/NavTabs/NavTabs.jsx
+++ client/src/components/NavTabs/NavTabs.jsx
@@ -3,12 +3,12 @@ import "./NavTabs.css";
 
 const NavTabs = () => {
   return (
-    <div class="col s12">
-      <ul class="tabs tabs-icon">
-        <li class="tab col s3"><a href="/developers"><i class="material-icons">people</i>Developers</a></li>
-        {/* <li class="tab col s3"><a href="#test2"><i class="material-icons">computer</i>Projects</a></li> */}
-        <li class="tab col s3"><a href="/profile"><i class="material-icons">account_box</i>Profile</a></li>
-        <li class="tab col s3 right"><a href="#test4"><i class="material-icons">close</i>Sign Out</a></li>
+    <div className="col s12">
+      <ul className="tabs tabs-icon">
+        <li className="tab col s3"><a href="/developers"><i className="material-icons">people</i>Developers</a></li>
+        {/* <li className="tab col s3"><a href="#test2"><i className="material-icons">computer</i>Projects</a></li> */}
+        <li className="tab col s3"><a href="/profile"><i className="material-icons">account_box</i>Profile</a></li>
+        <li className="tab col s3 right"><a href="#test4"><i className="material-icons">close</i>Sign Out</a></li>
       </ul>
     </div>
   );
