79d1ac56bdcae94a2e1dda059e89511f5ea5c055
Elyse Wells <cewells89@gmail.com>
2021-02-13 14:15:23 -0500

    updated navtab routes. added token expiration.

--- client/src/components/NavTabs/NavTabs.jsx
+++ client/src/components/NavTabs/NavTabs.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./NavTabs.css";
+import {NavLink} from "react-router-dom";
 
 const NavTabs = () => {
   return (
     <div className="col s12">
       <ul className="tabs tabs-icon">
-        <li className="tab col s3"><a href="/developers"><i className="material-icons">people</i>Developers</a></li>
+        <li className="tab col s3"><NavLink to="/developers"><i className="material-icons">people</i>Developers</NavLink></li>
         {/* <li className="tab col s3"><a href="#test2"><i className="material-icons">computer</i>Projects</a></li> */}
-        <li className="tab col s3"><a href="/profile"><i className="material-icons">account_box</i>Profile</a></li>
-        <li className="tab col s3 right"><a href="#test4"><i className="material-icons">close</i>Sign Out</a></li>
+        <li className="tab col s3"><NavLink to="/profile"><i className="material-icons">account_box</i>Profile</NavLink></li>
+          
+          
+        <li className="tab col s3 right"><NavLink to="#"><i className="material-icons">close</i>Sign Out</NavLink></li>
       </ul>
     </div>
   );
