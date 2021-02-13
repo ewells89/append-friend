1c357c45d5072822ae2e3782dfc329faeb3c219b
Elyse Wells <cewells89@gmail.com>
2021-02-13 11:29:43 -0500

    added picture upload component on profile page.

---
+++ client/src/components/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ImageUploader from 'react-images-upload';
+require("react-dom/package.json");
+
+class ImageUpload extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { pictures: [] };
+        this.onDrop = this.onDrop.bind(this);
+    }
+
+    onDrop(pictureFiles, pictureDataURLs) {
+        this.setState({
+            pictures: pictureFiles
+        });
+    }
+
+    render() {
+        return (
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+                singleImage={true}
+                withPreview={true}
+            />
+        );
+    }
+}
+
+
+export default ImageUpload;
