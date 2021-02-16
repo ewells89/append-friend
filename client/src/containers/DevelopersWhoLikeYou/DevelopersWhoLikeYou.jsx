05c2045fff38041f3a8b6fffcdf44f8e9a37cf5e
Elyse Wells <cewells89@gmail.com>
2021-02-15 19:36:55 -0500

    added additional dev page containers. imported to app.js file. added links to navbar component. removed hard coded values from devcards component.

---
+++ client/src/containers/DevelopersWhoLikeYou/DevelopersWhoLikeYou.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import DevCards from "../../components/DevCards/DevCards";
+import axios from "axios";
+
+const DevelopersWhoLikeYou = () => {
+  const [developers, setDevelopers] = useState([]);
+
+  useEffect(() => {
+    getDevs()
+    // getDevsWhoLikeYou()
+  }, []);
+
+  // const getDevsWhoLikeYou = () => {};
+  const getDevs = () => {
+    axios
+    .get("/api/developer")
+    .then((response) => {
+      console.log(response.data);
+      setDevelopers(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          {developers.map((developer) => {
+            return <DevCards {...developer} key={developer.username} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DevelopersWhoLikeYou;
---
+++ client/src/containers/LikedDevelopers/LikedDevelopers.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import DevCards from "../../components/DevCards/DevCards";
+import axios from "axios";
+
+const LinkedDevelopers = () => {
+  const [developers, setDevelopers] = useState([]);
+
+  useEffect(() => {
+    getDevs()
+    // getLikedDevs()
+  }, []);
+
+  // const getLikedDevs = () => {};
+
+  const getDevs = () => {
+    axios
+    .get("/api/developer")
+    .then((response) => {
+      console.log(response.data);
+      setDevelopers(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          {developers.map((developer) => {
+            return <DevCards {...developer} key={developer.username} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LinkedDevelopers;
