4153e5303d7617845df20edb842f5bcf1ae9d9f5
Elyse Wells <cewells89@gmail.com>
2021-02-15 19:17:56 -0500

    modified card body props and styling.

--- client/src/components/DevCards/DevCards.jsx
+++ client/src/components/DevCards/DevCards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './DevCards.css';
 // import PropTypes from "prop-types";
 
-const DevCards = ({username,name,gitHub,languages,phone,experience,location,occupation,appRole,}) => {
+const DevCards = ({username,name,gitHub,description,phone,experience,location,occupation,appRole,}) => {
     return (
         
         <div className="col s4">
@@ -15,20 +15,29 @@ const DevCards = ({username,name,gitHub,languages,phone,experience,location,occu
                     </div>
                 
                     <div className="col s7" id="devCardDevNameColumn">
-                        <div id="devCardDevName">
-                            <p>{name}</p>
+                        <div className="row">
+                            <div id="devCardDevName">
+                                <p className="devCardP">{name}</p>
+                            </div>
+                            <div id="devCardDevOccupation">
+                                <p className="devCardP">Software Engineer
+                                    {occupation}
+                                    </p>
+                            </div>
+                            <div id="devCardDevRole">
+                                <p className="devCardP">Mentor
+                                    {appRole}
+                                    </p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="card-content" id="devCardDevDescription">
-                        <p>Description of the developer or some other content will go here.</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="card-content" id="devCardDevLanguages">
-                        <p>{languages}</p>
-                        <a className="btn-floating  waves-effect waves-light black" id="devCardLikeButton"><i className="material-icons">favorite</i></a>
+                        <p>Description of the developer or some other content will go here.
+                            {description}
+                        </p>
+                        <a className="btn-floating waves-effect waves-light grey lighten-3" id="devCardLikeButton"><i className="material-icons pink-text text-accent-1">favorite</i></a>
                     </div>
                 </div>
             </div>
